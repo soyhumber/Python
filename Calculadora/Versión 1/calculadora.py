b6dae5f3fc2719ef5e8afc1587e8855b359cfd88
soyhumber <humbertovava@gmail.com>
2020-04-19 17:14:30 -0600

    Add files via upload

---
+++ Calculadora/Versión 1/calculadora.py
@@ -0,0 +1,19 @@
+def suma(a, b) :
+    resultado = a + b
+    return resultado
+
+def resta(a, b) :
+    resultado = a - b
+    return resultado
+
+def multiplicacion(a, b) :
+    resultado = a * b
+    return resultado
+
+def division(a, b) :
+    if (b == 0) :
+        # Hacer algo
+        print("Error!")
+    else :
+        resultado = a / b
+        return resultado
